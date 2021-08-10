eb76e73c90bcd323ce76f2780bc1be162d816faa
Kaique <motamendes.kaique.100@gmail.com>
2021-08-10 19:50:30 -0300

    finishing the search bar

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Product
-# Create your views here.
+
 
 class ProductsDetailView(DetailView):
     model = Product
@@ -16,3 +16,13 @@ class ProductListView(ListView):
     
     def get_queryset(self):
         return Product.objects.filter(is_available=True)
+
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        searchbar = self.request.GET.get("searchbar") or ''
+        if searchbar:
+            context["product"] = context["product"].filter(name__istartswith=searchbar)
+        context["searchbar"] = searchbar
+        
+        return context
