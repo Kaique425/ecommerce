4d29e8705505f0b80e89176b01304f88a3b44db1
Kaique <motamendes.kaique.100@gmail.com>
2021-08-11 19:51:28 -0300

    styling the product card and image

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Product (models.Model):
     name = models.CharField(max_length=64)
+    product_image = models.ImageField(upload_to= 'products/images',blank=True,null=True)
     price = models.DecimalField(max_digits=999, decimal_places=2)
     description = models.TextField(blank=True, null=True)
     is_available = models.BooleanField(default=True)
