d086eb85962c483a8481ece367b88097256544ab
Kaique <motamendes.kaique.100@gmail.com>
2021-10-28 05:04:12 -0300

    Public key bug

--- payments/views.py
+++ payments/views.py
@@ -12,14 +12,24 @@ class PaymentCreate(CreateView):
     form_class = PaymentForm
     template_name = 'payments/create.html'
 
-    @cached_property    
+    @cached_property
     def order(self):
-        order_id = self.request.session.get('order_id')
+        order_id = self.request.session.get("order_id")
         order = get_object_or_404(Orders, id=order_id)
         return order
+
+    def get_form_kwargs(self):
+        form_kwargs = super().get_form_kwargs()
+        form_kwargs["order"] = self.order
+        return form_kwargs
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['PUBLIC_KEY'] = settings.MERCADO_PAGO_PUBLIC_KEY
         return context
 
+    def form_valid(self, form):
+        form.save()
+
+        return super().form_valid(form)
